d82e645e0f0f0095261b33471abde02005f95a4b
Josh Gillette <joshtgillette@gmail.com>
2025-06-13 14:43:57 -0500

    Use Gorilla for routing

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.24.1
 
 require (
 	github.com/aws/aws-lambda-go v1.48.0 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,43 @@
 package main
 
 import (
-	"strings"
+	"fmt"
+	"net/http"
+	"os"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	adapter "github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
-func handler(request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-	path := strings.TrimPrefix(request.RawPath, "/release")
+func taglineHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Generated ui for <i>you</i>, coming soon"))
+}
 
-	switch path {
-	case "/tagline":
-		return events.APIGatewayV2HTTPResponse{
-			StatusCode: 200,
-			Body:       "Generated ui for <i>you</i>, coming soon",
-		}, nil
-	default:
-		return events.APIGatewayV2HTTPResponse{
-			StatusCode: 404,
-			Body:       "Page not found",
-		}, nil
-	}
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Page not found"))
 }
 
 func main() {
-	lambda.Start(handler)
+	_ = godotenv.Load() // Loads .env file if present
+
+	r := mux.NewRouter()
+	r.HandleFunc("/tagline", taglineHandler).Methods("GET")
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	if os.Getenv("LOCAL") == "1" {
+		// Run as a local HTTP server for development
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8080"
+		}
+		fmt.Printf("\nStarting local server at http://localhost:%s\n\n", port)
+		panic(http.ListenAndServe(":"+port, r))
+	} else {
+		// Run as AWS Lambda
+		lambda.Start(adapter.New(r).ProxyWithContext)
+	}
 }
